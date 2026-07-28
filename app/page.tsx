30f61a9c88c6a68360762f6b9e62191809cae00d
Nima <nima.abed.ch@gmail.com>
2026-07-28 16:37:05 +0330

    desktop slider template added!

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,22 @@ import Slider from '@/components/Slider';
 
 export default function ProductPage() {
   return (
-    <main className="w-full grid md:grid-cols-2">
-      {/* Image Slider */}
-      <section className="w-full">
-        <Slider />
+    <>
+      <div className="hidden w-full h-[3px] bg-light-grayish-blue mt-4 mb-10 rounded-full md:block" />
+      <main className="w-full grid md:grid-cols-2 md:gap-x-10 md:py-8">
+        {/* Image Slider */}
+        <section className="w-full">
+          <Slider />
+        </section>
+
+        {/* Article Section */}
+        <section className="w-full h-[200px] bg-blue-500">
+
+
+        </section>
         
-      </section>
-
-      {/* Article Section */}
-      <section className="w-full h-[200px] bg-blue-500">
-
-
-      </section>
       
-    
-    </main>
+      </main>
+    </>
   );
 }
--- components/Slider.tsx
+++ components/Slider.tsx
@@ -3,30 +3,42 @@ import Image from "next/image";
 
 export default function Slider() {
   return (
-    <div className="relative h-[300px]">
-      <Image 
-        className="object-cover"
-        src='/image-product-1.jpg' 
-        alt='shoes image' 
-        fill
-      />
-      <div className="absolute w-10 h-10 flex justify-center items-center bg-white right-4 top-1/2 -translate-y-1/2 rounded-full p-3 cursor-pointer md:hidden">
+    <div className="w-full md:max-w-[70%] md:mx-auto">
+      <div className="relative h-[300px] md:h-[500px]">
         <Image 
-          src="/icon-next.svg" 
-          alt="next icon image" 
-          width={12}
-          height={12}
+          className="object-cover md:rounded-xl"
+          src='/image-product-1.jpg' 
+          alt='shoes image' 
+          fill
         />
+        <div className="absolute w-10 h-10 flex justify-center items-center bg-white right-4 top-1/2 -translate-y-1/2 rounded-full p-3 cursor-pointer md:hidden">
+          <Image 
+            src="/icon-next.svg" 
+            alt="next icon image" 
+            width={12}
+            height={12}
+          />
+        </div>
+        <div className="absolute w-10 h-10 flex justify-center items-center bg-white left-4 top-1/2 -translate-y-1/2 rounded-full p-3 cursor-pointer md:hidden">
+          <Image 
+            className="mr-1"
+            src="/icon-previous.svg" 
+            alt="next icon image" 
+            width={11}
+            height={11}
+          />
+        </div>
       </div>
-      <div className="absolute w-10 h-10 flex justify-center items-center bg-white left-4 top-1/2 -translate-y-1/2 rounded-full p-3 cursor-pointer md:hidden">
-        <Image 
-          className="mr-1"
-          src="/icon-previous.svg" 
-          alt="next icon image" 
-          width={11}
-          height={11}
+
+      <div className="hidden flex-row justify-between mt-7 md:flex">
+        <div 
+          className="size-25 bg-[url(/image-product-1-thumbnail.jpg)] bg-cover rounded-lg"
         />
+        <div className="size-25 bg-[url(/image-product-2-thumbnail.jpg)] bg-cover rounded-lg"></div>
+        <div className="size-25 bg-[url(/image-product-3-thumbnail.jpg)] bg-cover rounded-lg"></div>
+        <div className="size-25 bg-[url(/image-product-4-thumbnail.jpg)] bg-cover rounded-lg"></div>
       </div>
+
     </div>
   )
 }
