fcd2d1b93441559764fda71f472b9ced7d03598a
Nima <nima.abed.ch@gmail.com>
2026-09-15 20:46:15 +0330

    filter result and selected filter section added!

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -17,8 +17,9 @@ const buttonVariants = cva(
           "hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:hover:bg-muted/50",
         destructive:
           "bg-destructive/10 text-destructive hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40",
-        link: "text-primary underline-offset-4 hover:underline",
+        link: "text-dark-grayish-blue underline-offset-4 hover:underline",
         active: "bg-black text-white",
+        label: "bg-dark-grayish-blue/10 rounded-full hover:bg-dark-grayish-blue/30"
       },
       size: {
         default:
--- pages/products/Filter.tsx
+++ pages/products/Filter.tsx
@@ -3,7 +3,7 @@ import { useRef, useState } from 'react';
 import Container from '@/components/ui/container';
 import Modal, {type modalRef } from '@/components/ui/Modal';
 import { Button } from '@/components/ui/button';
-import { Funnel, ChevronDown } from 'lucide-react';
+import { Funnel, ChevronDown, X } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import FilterContents from './FilterContents';
 import SortContents from './SortContents';
@@ -45,21 +45,44 @@ export default function Filter() {
           {content === 'sort' && <SortContents onClose={() => closeModal()} />}
         </div>
       </Modal>
-      <Container className="flex items-center justify-between">
-        <Button 
-          className="bg-white py-2 px-5 border-dark-grayish-blue/20 font-semibold cursor-pointer active:translate-y-0!"
-          onClick={filterButtonHandler}
-        >
-          <Funnel className="text-primary" />
-          Filters
-        </Button>
-        <Button 
-          className="bg-white py-2 px-5 border-dark-grayish-blue/20 font-semibold cursor-pointer active:translate-y-0!"
-          onClick={sortButtonHandler}  
-        >
-          Sort
-          <ChevronDown />  
-        </Button>
+      <Container className="space-y-2 py-5">
+        {/* Filter/Sort Buttons Sections */}
+        <div className="flex items-center justify-between">
+          <Button 
+            variant="outline"
+            className="py-2 px-6 font-semibold "
+            onClick={filterButtonHandler}
+          >
+            <Funnel className="text-primary" />
+            Filters
+          </Button>
+          <Button 
+            variant="outline"
+            className="py-2 px-6 font-semibold "
+            onClick={sortButtonHandler}  
+          >
+            Sort
+            <ChevronDown />  
+          </Button>
+        </div>
+        {/* Result Section */}
+        <div>
+          <span className="text-sm text-dark-grayish-blue">Showing 1-12 of 48 products</span>
+        </div>
+        {/* Selected filters actions */}
+        <div className="space-x-2">
+          <Button variant="label" size="lg">
+            Men
+            <X className="text-dark-grayish-blue" />
+          </Button>
+          <Button variant="label" size="lg">
+            Sneakers
+            <X className="text-dark-grayish-blue" />
+          </Button>
+          <Button variant="link">
+            Clear all
+          </Button>
+        </div>
       </Container>
     </>
   )
