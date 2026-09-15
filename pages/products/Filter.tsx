69813bc2b8125588b43c8e150b9202e1589ce132
Nima <nima.abed.ch@gmail.com>
2026-09-15 20:15:15 +0330

    sort modal added!

--- pages/products/Filter.tsx
+++ pages/products/Filter.tsx
@@ -6,10 +6,12 @@ import { Button } from '@/components/ui/button';
 import { Funnel, ChevronDown } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import FilterContents from './FilterContents';
+import SortContents from './SortContents';
 
 export default function Filter() {
 
   const dialogRef = useRef<modalRef>(null);
+  const [content, setContent] = useState('');
   const [isClosing, setIsClosing] = useState(false);
 
   function closeModal() {
@@ -18,6 +20,16 @@ export default function Filter() {
       dialogRef.current?.close();
       setIsClosing(false);
     }, 250)
+  }
+
+  function filterButtonHandler() {
+    setContent('filter');
+    dialogRef.current?.open();
+  }
+
+  function sortButtonHandler() {
+    setContent('sort');
+    dialogRef.current?.open();
   }
 
   return (
@@ -29,18 +41,22 @@ export default function Filter() {
         <div className={cn("absolute size-full bottom-0 duration-300 bg-white", 
           isClosing ? "animate-out slide-out-to-bottom" : "animate-in slide-in-from-bottom"
         )}>
-          <FilterContents onClose={() => closeModal()} />
+          {content === 'filter' && <FilterContents onClose={() => closeModal()} />}
+          {content === 'sort' && <SortContents onClose={() => closeModal()} />}
         </div>
       </Modal>
       <Container className="flex items-center justify-between">
         <Button 
           className="bg-white py-2 px-5 border-dark-grayish-blue/20 font-semibold cursor-pointer active:translate-y-0!"
-          onClick={() => dialogRef.current?.open()}
+          onClick={filterButtonHandler}
         >
           <Funnel className="text-primary" />
           Filters
         </Button>
-        <Button className="bg-white py-2 px-5 border-dark-grayish-blue/20 font-semibold cursor-pointer active:translate-y-0!">
+        <Button 
+          className="bg-white py-2 px-5 border-dark-grayish-blue/20 font-semibold cursor-pointer active:translate-y-0!"
+          onClick={sortButtonHandler}  
+        >
           Sort
           <ChevronDown />  
         </Button>
---
+++ pages/products/SortContents.tsx
@@ -0,0 +1,66 @@
+import Container from '@/components/ui/container';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { Label } from '@/components/ui/label';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { X } from 'lucide-react';
+
+export default function SortContents({ onClose }: {
+  onClose: () => void
+}) {
+  return (
+    <>
+      {/* Title Section */}
+      <Container>
+        <div className="flex items-center justify-between py-3">
+          <h2 className="font-bold text-lg">Sort by</h2>
+          <Button
+            className="bg-transparent cursor-pointer -mr-2 hover:bg-dark-grayish-blue/20"
+            onClick={onClose}
+          >
+            <X strokeWidth={2} className="size-5" />
+          </Button>
+        </div>
+      </Container>
+      <Separator className="bg-dark-grayish-blue/20" />
+      {/* Selections */}
+      <Container>
+        <div className="py-4">
+          <RadioGroup defaultValue="featured" className="space-y-2">
+            <div className="flex items-center justify-between">
+              <Label htmlFor="featured" className="text-base font-bold">Featured</Label>
+              <RadioGroupItem value="featured" id="featured" />
+            </div>
+            <Separator className="bg-dark-grayish-blue/20" />
+            <div className="flex items-center justify-between">
+              <Label htmlFor="price-low-to-high" className="text-base text-dark-grayish-blue">Price: low to high</Label>
+              <RadioGroupItem value="price-low-to-high" id="price-low-to-high" />
+            </div>
+            <Separator className="bg-dark-grayish-blue/20" />
+            <div className="flex items-center justify-between">
+              <Label htmlFor="price-high-to-low" className="text-base text-dark-grayish-blue">Price: high to low</Label>
+              <RadioGroupItem value="price-high-to-low" id="price-high-to-low" />
+            </div>
+            <Separator className="bg-dark-grayish-blue/20" />
+            <div className="flex items-center justify-between">
+              <Label htmlFor="newest" className="text-base text-dark-grayish-blue">Newest arrivals</Label>
+              <RadioGroupItem value="newest" id="newest" />
+            </div>
+            <Separator className="bg-dark-grayish-blue/20" />
+            <div className="flex items-center justify-between">
+              <Label htmlFor="rate" className="text-base text-dark-grayish-blue">Best rated</Label>
+              <RadioGroupItem value="rate" id="rate" />
+            </div>
+          </RadioGroup>
+        </div>
+      </Container>
+      <Separator className="bg-dark-grayish-blue/20" />
+      {/* Action Section */}
+      <Container>
+        <div className="py-4">
+          <Button className="w-full font-bold text-base text-white py-5">Apply</Button>
+        </div>
+      </Container>
+    </>
+  )
+}
